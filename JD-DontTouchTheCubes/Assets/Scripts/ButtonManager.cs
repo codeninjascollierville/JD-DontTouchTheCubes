77083e627a69695253fd4e056ce4f187de88035f
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-07-22 12:44:46 -0500

    7/22/23
    step 36

---
+++ JD-DontTouchTheCubes/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ JD-DontTouchTheCubes/Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject timerCanvas;
+
+    private Text timerText;
+    private int timerCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerCanvas.SetActive(true);
+        StartCoroutine(CountTime());
+        Time.timeScale = 1f;
+        timerText = GameObject.Find("Score").GetComponent<Text>();
+    }
+
+    IEnumerator CountTime()
+    {
+        yield return new WaitForSeconds(1f);
+        timerCount++;
+        timerText.text = "Score: " + timerCount;
+        StartCoroutine(CountTime());
+    }
+}
